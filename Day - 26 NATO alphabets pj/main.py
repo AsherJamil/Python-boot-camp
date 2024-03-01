af993361609d99aeca8368a3521685c7caea9dc1
AsherJamil <asher.rjamil@gmail.com>
2024-03-01 14:49:45 +0500

    Day - 26

--- Day - 26 NATO alphabets pj/main.py
+++ Day - 26 NATO alphabets pj/main.py
@@ -6,6 +6,15 @@ data = pandas.read_csv("/python 100 days bootcamp/Github.git/Day - 26 NATO alpha
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry only letters are allowed")
+        generate_phonetic()
+    else:
+        print(output_list)
+        
+generate_phonetic()
