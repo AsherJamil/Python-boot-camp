7476dee3fadbc887f040635cbbb8eac472c7ecc5
AsherJamil <153119071+AsherJamil@users.noreply.github.com>
2024-06-05 15:22:09 +0500

    Day - 53

---
+++ Day - 53 Zillow San Fran Rentals Debugged.py
@@ -0,0 +1,72 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from chromedriver_py import binary_path
+
+# Create an instance of Options class
+options = Options()
+
+# Add Chrome driver path to options
+options.binary_location = binary_path
+
+# Pass options to webdriver.Chrome() constructor
+driver = webdriver.Chrome(options=options)
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+response = requests.get(
+    "https://www.zillow.com/homes/San-Francisco,-CA_rb/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22San%20Francisco%2C%20CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-122.55177535009766%2C%22east%22%3A-122.31488264990234%2C%22south%22%3A37.69926912019228%2C%22north%22%3A37.851235694487485%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D",
+    headers=header)
+
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+all_link_elements = soup.select(".list-card-top a")
+all_links = []
+for link in all_link_elements:
+    href = link["href"]
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
+all_address_elements = soup.select(".list-card-info address")
+all_addresses = [address.get_text().split(" | ")[-1] for address in all_address_elements]
+
+all_price_elements = soup.select(".list-card-heading")
+all_prices = []
+for element in all_price_elements:
+    try:
+        price = element.select(".list-card-price")[0].contents[0]
+    except IndexError:
+        price = element.select(".list-card-details li")[0].contents[0]
+    finally:
+        all_prices.append(price)
+
+for n in range(len(all_links)):
+    # Substitute your own Google Form URL here 👇
+    driver.get('URL_TO_YOUR_GOOGLE_FORM')
+    
+    try:
+        address_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+        price_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+        link_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+        submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')))
+        
+        address_input.send_keys(all_addresses[n])
+        price_input.send_keys(all_prices[n])
+        link_input.send_keys(all_links[n])
+        submit_button.click()
+        time.sleep(2)  # Add a delay to avoid overwhelming the server
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+driver.quit()
