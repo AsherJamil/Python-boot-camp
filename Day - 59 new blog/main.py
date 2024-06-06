52c0060ee404b92e5f93e731fd049393797b48b9
AsherJamil <153119071+AsherJamil@users.noreply.github.com>
2024-06-06 14:02:48 +0500

    Day - 59

--- Day - 59 new blog/main.py
+++ Day - 59 new blog/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 import requests
 
-# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
