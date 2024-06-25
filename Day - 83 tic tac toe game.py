4f3e76c4654af53025989929ba1caac812c1c7d5
AsherJamil <153119071+AsherJamil@users.noreply.github.com>
2024-06-25 14:20:16 +0500

    Day - 83

---
+++ Day - 83 tic tac toe game.py
@@ -0,0 +1,60 @@
+def print_board(board):
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
+    print("---|---|---")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("---|---|---")
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
+
+
+def check_winner(board, mark):
+    win_conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for condition in win_conditions:
+        if board[condition[0]] == board[condition[1]] == board[condition[2]] == mark:
+            return True
+    return False
+
+
+def check_draw(board):
+    return all(space != ' ' for space in board)
+
+
+def player_move(board, player):
+    while True:
+        try:
+            move = int(input(f"Player {player}, enter your move (1-9): ")) - 1
+            if board[move] == ' ':
+                board[move] = player
+                break
+            else:
+                print("That space is already taken. Try again.")
+        except (IndexError, ValueError):
+            print("Invalid input. Please enter a number between 1 and 9.")
+
+
+def main():
+    board = [' ' for _ in range(9)]
+    current_player = 'X'
+
+    while True:
+        print_board(board)
+        player_move(board, current_player)
+
+        if check_winner(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+
+        if check_draw(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+if __name__ == "__main__":
+    main()
